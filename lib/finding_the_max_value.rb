e00f7a03237e3c762c84e00da6defe2f090abd94
Miguel  Arreola <marreola4619@gmail.com>
2020-11-09 22:47:00 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,7 +1,11 @@
 def find_max_value(array)
   count = 0 
-  while count < array.lenght do 
-    puts array[count] == find_max_value
+  max_value = -1
+  while count < array.length do 
+    if max_value < array[count]
+      max_value = array[count]
+    end
     count += 1 
-end
+  end
+  max_value
 end
